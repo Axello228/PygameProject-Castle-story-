0d352774d3eebec4e6db9d43433a2c35b0e8e79d
Axelle228 <a.d.kubrin@yandex.ru>
2022-01-14 10:32:35 -0800

    Initial Commit

--- main.py
+++ main.py
@@ -38,17 +38,19 @@ class Board:
         for i in range(17):
             for j in range(32):
                 if lst_map_the_isle[i][j] == 2:
-                    self.location_my_castle = (i, j)
+                    self.location_my_castle = (j, i)
                 if lst_map_the_isle[i][j] == 3:
-                    self.location_bot_castle = (i, j)
+                    self.location_bot_castle = (j, i)
         self.player1 = [10, 0, 0]
         self.builds_player1 = []
+        self.edifice_player1 = []
         self.player2 = [0, 0, 0]
         self.builds_player2 = []
+        self.edifice_player2 = []
         self.motion = 1
         self.alchemistry_stage = 0
 
-    def get_plyer(self):
+    def get_player(self):
         if self.motion == 1:
             return self.player1
         else:
@@ -64,18 +66,51 @@ class Board:
         if game.return_action_stage() == "map_VS":
             if self.motion == 1:
                 self.motion = 2
+                i = 0
+                while i < len(self.builds_player2):
+                    if self.builds_player2[i][1] > 0:
+                        self.builds_player2[i][1] -= 1
+                    if self.builds_player2[i][1] == 0:
+                        lst_map_the_isle[self.builds_player2[i][2][1]][self.builds_player2[i][2][0]] = self.builds_player2[i][0]
+                        self.edifice_player2.append(self.builds_player2[i][0])
+                        self.builds_player2[i], self.builds_player2[-1] = self.builds_player2[-1], self.builds_player2[i]
+                        self.builds_player2.pop()
+                    i += 1
             else:
                 self.motion = 1
+                i = 0
+                while i < len(self.builds_player1):
+                    if self.builds_player1[i][1] > 0:
+                        self.builds_player1[i][1] -= 1
+                    if self.builds_player1[i][1] == 0:
+                        lst_map_the_isle[self.builds_player1[i][2][1]][self.builds_player1[i][2][0]] = self.builds_player1[i][0]
+                        self.edifice_player1.append(self.builds_player1[i][0])
+                        self.builds_player1[i], self.builds_player1[-1] = self.builds_player1[-1], self.builds_player1[i]
+                        self.builds_player1.pop()
+                    i += 1
 
     def build(self, what_build):
         if self.motion == 1:
-            pass
+            self.builds_player1.append([what_build, self.get_how_build(), game.get_pos()])
         else:
-            pass
+            self.builds_player2.append([what_build, self.get_how_build(), game.get_pos()])
+
+    def get_how_build(self):
+        pos = game.get_pos()
+        if self.motion == 2:
+            return max(abs(self.location_bot_castle[0] - pos[0]), abs(self.location_bot_castle[1] - pos[1]))
+        else:
+            return max(abs(self.location_my_castle[0] - pos[0]), abs(self.location_my_castle[1] - pos[1]))
 
 
 class Game:
     def __init__(self):
+        self.lst_textures = ["textures\\field.png", "textures\\forest.png", "textures\my_castle.png", "textures\\bot_castle.png",
+                "textures\\north_beach.png", "textures\south_beach.png", "textures\east_beach.png",
+                "textures\west_beach.png", "textures\\nw_beach.png", "textures\\ne_beach.png", "textures\se_beach.png",
+                "textures\sw_beach.png", "textures\\bot_swordsman_r.png", "textures\\bot_swordsman_l.png",
+                "textures\my_swordsman_r.png", "textures\my_swordsman_l.png", "textures\house.png", "textures\sawmill.png",
+                "textures\\alchemistry.png", "textures\mine.png", "textures\smithy.png", "textures\\byilding_table.png"]
         self.action_stage = "main_menu"
         self.render_functions = {"main_menu": self.render_home_screen, "mode_selection": self.render_mode_selection_screen,
                     "map_VS": self.render_map_VS}
@@ -118,11 +153,11 @@ class Game:
         for i in range(17):
             x = 0
             for j in range(32):
-                screen.blit(pygame.image.load(lst_textures[lst_map_the_isle[i][j]]), (x, y))
+                screen.blit(pygame.image.load(self.lst_textures[lst_map_the_isle[i][j]]), (x, y))
                 x += 60
             y += 60
         x, y = 600, 0
-        lst_player = board.get_plyer()
+        lst_player = board.get_player()
         for i in range(3):
             pygame.draw.rect(screen, self.active_clr, (x, y, 150, 50))
             game.print_text(40, str(lst_player[i]), (0, 0, 0), (x, y))
@@ -130,7 +165,6 @@ class Game:
 
         if self.is_construction_window:
             game.render_construction_window(self.selection_cell)
-
 
     def render_mode_selection_screen(self):
         screen.blit(main_screen, (0, 0))
@@ -152,7 +186,7 @@ class Game:
             screen.blit(construction_window_screen, (0, 0))
             cout = 0
             for i in range(16, 21):
-                screen.blit(pygame.image.load(lst_textures[i]), (20, 20 + cout))
+                screen.blit(pygame.image.load(self.lst_textures[i]), (20, 20 + cout))
                 cout += 100
             game.draw_button(70, 50, 100, 30, "Дом", "house")
             game.draw_button(150, 50, 100, 130, "Лесопилка", "sawmill")
@@ -171,11 +205,12 @@ class Game:
         pygame.mixer.music.unpause()
 
     def render_build(self, what_build):
-        lst_map_the_isle[self.selection_cell[1]][self.selection_cell[0]] = 16
+        self.is_construction_window = False
+        lst_map_the_isle[self.selection_cell[1]][self.selection_cell[0]] = 21
         board.build(what_build)
 
-
-
+    def get_pos(self):
+        return self.selection_cell
 
 lst_map_the_isle = [[8, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 9],
          [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 6],
@@ -194,17 +229,6 @@ lst_map_the_isle = [[8, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
          [7, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 6],
          [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6],
          [11, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10]]
-
-
-
-
-lst_textures = ["textures\\field.png", "textures\\forest.png", "textures\my_castle.png", "textures\\bot_castle.png",
-                "textures\\north_beach.png", "textures\south_beach.png", "textures\east_beach.png",
-                "textures\west_beach.png", "textures\\nw_beach.png", "textures\\ne_beach.png", "textures\se_beach.png",
-                "textures\sw_beach.png", "textures\\bot_swordsman_r.png", "textures\\bot_swordsman_l.png",
-                "textures\my_swordsman_r.png", "textures\my_swordsman_l.png", "textures\house.png", "textures\sawmill.png",
-                "textures\\alchemistry.png", "textures\mine.png", "textures\smithy.png"]
-
 
 main_screen = pygame.transform.scale(pygame.image.load(r"textures\background.jpg"), (1920, 1080))
 construction_window_screen = pygame.transform.scale(pygame.image.load(r"textures\background_construction_window.PNG"), (400, 1080))
