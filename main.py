950521605905c04df95735b6139ceed41ac77949
Axelle228 <a.d.kubrin@yandex.ru>
2022-01-18 08:39:30 -0800

    Initial Commit

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ class Board:
     def house(self, pos):
         pass
 
-    def return_alchemistry_stage(self):
+    def get_alchemistry_stage(self):
         if self.motion == 1:
             return self.alchemistry_stage_player1
         else:
@@ -132,7 +132,7 @@ class Board:
             for i in range(len(build)):
                 self.player2[i] -= build[i]
 
-    def return_alchemistry_cost(self):
+    def get_alchemistry_cost(self):
         return self.alchemistry_cost
 
 class Game:
@@ -149,7 +149,7 @@ class Game:
         self.action_stage = "main_menu"
         self.render_functions = {"main_menu": self.render_home_screen, "mode_selection": self.render_mode_selection_screen,
                     "map_VS": self.render_map_VS, "settings": self.render_settings_window}
-        self.building = {"house": [16, 20, 0,50], "sawmill": [17, 5, 0, 0], "alchemistry": [18, 10, 0, 0],
+        self.building = {"house": [16, 20, 0, 5], "sawmill": [17, 5, 0, 0], "alchemistry": [18, 10, 0, 0],
                          "mine": [19, 15, 0, 0], "smithy": [20, 20, 10, 0]}
         self.settings = {"on_music": self.on_soundtrack, "off_music": self.off_sondtrack, "on_sounds": self.on_sounds,
                          "off_sounds": self.off_sounds}
@@ -163,6 +163,7 @@ class Game:
         self.exit = False
         self.is_smithy_window = False
         self.smithy_pos = (0, 0)
+        self.board = Board()
 
     def exit(self):
         self.exit = True
@@ -174,29 +175,30 @@ class Game:
         self.is_alchemistry_window = False
 
     def up_alchemistry(self):
-        board.up_alchemistry()
+        self.board.up_alchemistry()
 
     def click(self):
-        if game.return_action_stage() == "map_VS":
+        if self.return_action_stage() == "map_VS":
             if not self.is_construction_window:
-                self.selection_cell = board.get_cell()
+                self.selection_cell = self.board.get_cell()
                 self.is_construction_window = True
             elif pygame.mouse.get_pos()[0] > 400:
                 self.is_construction_window = False
-            pos = board.get_cell()
+            pos = self.board.get_cell()
             if map[pos[1]][pos[0]] == 18 and not self.is_alchemistry_window:
-                self.alchemistry_pos = board.get_cell()
+                self.alchemistry_pos = self.board.get_cell()
                 self.is_alchemistry_window = True
             if map[pos[1]][pos[0]] == 20 and not self.is_smithy_window:
-                self.smithy_pos = board.get_cell()
+                self.smithy_pos = self.board.get_cell()
                 self.is_smithy_window = True
+
 
     def render(self):
         self.render_functions[self.action_stage]()
         if self.is_esc and self.action_stage != "main_menu":
-            game.render_esc_window()
+            self.render_esc_window()
 
-    def draw_button(self, width, height, x, y, message, stage="None", font_size=30):
+    def render_button(self, width, height, x, y, message, stage="None", font_size=30): # Кнопки живут своей жизнью, лутне все клики запихнуть в функцию click
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
@@ -209,14 +211,21 @@ class Game:
                 if stage in self.render_functions:
                     self.action_stage = stage
                 if stage in self.building:
-                    board.cost_construction(self.building[stage][1:]) # нужно сделать проверку на то что игрок может построить это здание
-                    self.render_build(self.building[stage][0])
+                    is_building = True
+                    cost_building = self.building[stage][1:]
+                    resurses_player = self.board.get_player()
+                    for i in range(3):
+                        if resurses_player[i] < cost_building[i]:
+                            is_building = False
+                    if is_building:
+                        self.board.cost_construction(cost_building)
+                        self.render_build(self.building[stage][0])
                 if stage in self.settings:
                     self.settings[stage]()
                 if stage in self.actions_in_the_game:
                     self.actions_in_the_game[stage]()
 
-        game.print_text(font_size, message, (0, 0, 0), (x + 5, y + 5))
+        self.print_text(font_size, message, (0, 0, 0), (x + 5, y + 5))
 
     def return_action_stage(self):
         return self.action_stage
@@ -230,25 +239,25 @@ class Game:
                 x += 60
             y += 60
         x, y = 500, 0
-        lst_player = board.get_player()
+        lst_player = self.board.get_player()
         for i in range(3):
             pygame.draw.rect(screen, self.active_clr, (x, y, 250, 50))
-            game.print_text(40, self.resources[i] + str(lst_player[i]), (0, 0, 0), (x, y))
+            self.print_text(40, self.resources[i] + str(lst_player[i]), (0, 0, 0), (x, y))
             x += 300
 
         if self.is_construction_window:
-            game.render_construction_window(self.selection_cell)
+            self.render_construction_window(self.selection_cell)
         if self.is_alchemistry_window:
-            game.render_alchemistry_window()
+            self.render_alchemistry_window()
         if self.is_smithy_window:
-            game.render_smithy_window()
+            self.render_smithy_window()
 
     def render_esc_window(self):
         screen.blit(pygame.transform.scale(pygame.image.load(r"textures\scroll.png"), (450, 500)), (720, 240))
-        game.draw_button(240, 50, 825, 340, "Продолжить игру", "map_VS")
-        game.draw_button(240, 50, 825, 400, "Главное меню", "main_menu")
-        game.draw_button(240, 50, 825, 460, "Сохранить")
-        game.draw_button(240, 50, 825, 520, "Настройки", "settings")
+        self.render_button(240, 50, 825, 340, "Продолжить игру", "map_VS")
+        self.render_button(240, 50, 825, 400, "Главное меню", "main_menu")
+        self.render_button(240, 50, 825, 460, "Сохранить")
+        self.render_button(240, 50, 825, 520, "Настройки", "settings")
         click = pygame.mouse.get_pressed()
         mouse = pygame.mouse.get_pos()
         if click[0] == 1 and 825 < mouse[0] < 1015 and 340 < mouse[1] < 570:
@@ -259,20 +268,20 @@ class Game:
         screen.blit(pygame.transform.scale(pygame.image.load(r"textures\scroll.png"), (800, 1100)), (-70, -50))
         screen.blit(pygame.transform.scale(pygame.image.load(r"textures\scroll.png"), (800, 1100)), (560, -50))
         screen.blit(pygame.transform.scale(pygame.image.load(r"textures\scroll.png"), (800, 1100)), (1190, -50))
-        game.draw_button(135, 55, 250, 80, "1 VS 1", "map_VS", font_size=45)
-        game.draw_button(135, 55, 890, 80, "Waves", font_size=45)
-        game.draw_button(170, 55, 1500, 80, "Invasion", font_size=45)
-        game.print_text(30, "В разработке", (0, 0, 0), (110, 200))
-        game.print_text(30, "В разработке", (0, 0, 0), (740, 200))
-        game.print_text(30, "В разработке", (0, 0, 0), (1370, 200))
+        self.render_button(135, 55, 250, 80, "1 VS 1", "map_VS", font_size=45)
+        self.render_button(135, 55, 890, 80, "Waves", font_size=45)
+        self.render_button(170, 55, 1500, 80, "Invasion", font_size=45)
+        self.print_text(30, "В разработке", (0, 0, 0), (110, 200))
+        self.print_text(30, "В разработке", (0, 0, 0), (740, 200))
+        self.print_text(30, "В разработке", (0, 0, 0), (1370, 200))
 
     def render_home_screen(self):
         screen.blit(main_screen, (0, 0))
         screen.blit(pygame.transform.scale(pygame.image.load(r"textures\scroll.png"), (460, 400)), (750, 320))
-        game.draw_button(250, 45, 850, 400, "Начать новую игру", "mode_selection")
-        game.draw_button(250, 45, 850, 450, "Загрузить игру")
-        game.draw_button(250, 45, 850, 500, "Настройки", "settings")
-        game.draw_button(250, 45, 850, 550, "Выход", "exit")
+        self.render_button(250, 45, 850, 400, "Начать новую игру", "mode_selection")
+        self.render_button(250, 45, 850, 450, "Загрузить игру")
+        self.render_button(250, 45, 850, 500, "Настройки", "settings")
+        self.render_button(250, 45, 850, 550, "Выход", "exit")
 
     def print_text(self, size, message, color, location, fnt='serif'):
         screen.blit(pygame.font.SysFont(fnt, size).render(message, True, color), location)
@@ -281,7 +290,7 @@ class Game:
         if map[pos[1]][pos[0]] == 0:
             screen.blit(construction_window_screen, (0, 0))
             cout = 0
-            alchemistry = board.return_alchemistry_stage()
+            alchemistry = self.board.get_alchemistry_stage()
             if alchemistry == 0:
                 build = (16, 19)
             elif alchemistry == 1:
@@ -291,18 +300,18 @@ class Game:
             for i in range(build[0], build[1]):
                 screen.blit(pygame.image.load(self.lst_textures[i]), (20, 20 + cout))
                 cout += 100
-            game.draw_button(70, 50, 100, 30, "Дом", "house")
-            game.print_text(20, "Деревo: 20, Руда: 0, Железо: 5", (0, 0, 0), (110, 5))
-            game.draw_button(150, 50, 100, 130, "Лесопилка", "sawmill")
-            game.print_text(20, "Деревo: 5, Руда: 0, Железо: 0", (0, 0, 0), (110, 105))
-            game.draw_button(290, 50, 100, 230, "Алхимическая платка", "alchemistry")
-            game.print_text(20, "Деревo: 10, Руда: 0, Железо: 0", (0, 0, 0), (110, 205))
+            self.render_button(70, 50, 100, 30, "Дом", "house")
+            self.print_text(20, "Деревo: 20, Руда: 0, Железо: 5", (0, 0, 0), (110, 5))
+            self.render_button(150, 50, 100, 130, "Лесопилка", "sawmill")
+            self.print_text(20, "Деревo: 5, Руда: 0, Железо: 0", (0, 0, 0), (110, 105))
+            self.render_button(290, 50, 100, 230, "Алхимическая платка", "alchemistry")
+            self.print_text(20, "Деревo: 10, Руда: 0, Железо: 0", (0, 0, 0), (110, 205))
             if alchemistry > 0:
-                game.draw_button(90, 50, 100, 330, "Шахта", "mine")
-                game.print_text(20, "Деревo: 15, Руда: 0, Железо: 0", (0, 0, 0), (110, 305))
+                self.render_button(90, 50, 100, 330, "Шахта", "mine")
+                self.print_text(20, "Деревo: 15, Руда: 0, Железо: 0", (0, 0, 0), (110, 305))
             if alchemistry > 1:
-                game.draw_button(90, 50, 100, 430, "Кузня", "smithy")
-                game.print_text(20, "Деревo: 20, Руда: 10, Железо: 0", (0, 0, 0), (110, 405))
+                self.render_button(90, 50, 100, 430, "Кузня", "smithy")
+                self.print_text(20, "Деревo: 20, Руда: 10, Железо: 0", (0, 0, 0), (110, 405))
 
     def off_sondtrack(self):
         pygame.mixer.music.pause()
@@ -313,7 +322,7 @@ class Game:
     def render_build(self, what_build):
         self.is_construction_window = False
         map[self.selection_cell[1]][self.selection_cell[0]] = 21
-        board.build(what_build)
+        self.board.build(what_build)
 
     def get_pos(self):
         return self.selection_cell
@@ -321,12 +330,12 @@ class Game:
     def render_settings_window(self):
         screen.blit(main_screen, (0, 0))
         screen.blit(pygame.transform.scale(pygame.image.load(r"textures\scroll.png"), (580, 600)), (680, 150))
-        game.draw_button(240, 50, 850, 260, "Продолжить игру", "map_VS") # нет условия на отрисовку этой кнопки!!!
-        game.draw_button(240, 50, 850, 320, "Главное меню", "main_menu")
-        game.draw_button(240, 50, 850, 380, "Отключить музыку", "off_music")
-        game.draw_button(240, 50, 850, 440, "Включить музыку", "on_music")
-        game.draw_button(240, 50, 850, 500, "Отключить звук", "off_sounds")
-        game.draw_button(240, 50, 850, 560, "Включить звук", "on_sounds")
+        self.render_button(240, 50, 850, 260, "Продолжить игру", "map_VS") # нет условия на отрисовку этой кнопки!!!
+        self.render_button(240, 50, 850, 320, "Главное меню", "main_menu")
+        self.render_button(240, 50, 850, 380, "Отключить музыку", "off_music")
+        self.render_button(240, 50, 850, 440, "Включить музыку", "on_music")
+        self.render_button(240, 50, 850, 500, "Отключить звук", "off_sounds")
+        self.render_button(240, 50, 850, 560, "Включить звук", "on_sounds")
 
     def on_sounds(self):
         self.is_sounds = True
@@ -341,23 +350,25 @@ class Game:
             else:
                 self.is_esc = True
 
+    def course_change(self):
+        self.board.course_change()
+
     def render_alchemistry_window(self):
-        if board.return_alchemistry_stage() != 3:
+        if self.board.get_alchemistry_stage() != 3:
             screen.blit(pygame.transform.scale(pygame.image.load(r"textures\scroll.png"), (300, 350)), (self.alchemistry_pos[0] * 60 - 120, self.alchemistry_pos[1] * 60 - 120))
-            game.draw_button(120, 35, self.alchemistry_pos[0] * 60 - 40, self.alchemistry_pos[1] * 60 - 40, "Улучшить", "up_alchemistry", font_size=25)
-            game.print_text(25, "Стоимость:", (0, 0, 0), (self.alchemistry_pos[0] * 60 - 40, self.alchemistry_pos[1] * 60))
-            game.print_text(25, "Уровень: " + str(board.return_alchemistry_stage()), (0, 0, 0), (self.alchemistry_pos[0] * 60 - 40, self.alchemistry_pos[1] * 60 + 80))
-            game.print_text(25, str(board.return_alchemistry_cost()[board.return_alchemistry_stage()])[1:-1], (0, 0, 0), (self.alchemistry_pos[0] * 60 - 40, self.alchemistry_pos[1] * 60 + 40))
+            self.render_button(120, 35, self.alchemistry_pos[0] * 60 - 40, self.alchemistry_pos[1] * 60 - 40, "Улучшить", "up_alchemistry", font_size=25)
+            self.print_text(25, "Стоимость:", (0, 0, 0), (self.alchemistry_pos[0] * 60 - 40, self.alchemistry_pos[1] * 60))
+            self.print_text(25, "Уровень: " + str(self.board.get_alchemistry_stage()), (0, 0, 0), (self.alchemistry_pos[0] * 60 - 40, self.alchemistry_pos[1] * 60 + 80))
+            self.print_text(25, str(self.board.get_alchemistry_cost()[self.board.get_alchemistry_stage()])[1:-1], (0, 0, 0), (self.alchemistry_pos[0] * 60 - 40, self.alchemistry_pos[1] * 60 + 40))
         else:
             screen.blit(pygame.transform.scale(pygame.image.load(r"textures\scroll.png"), (300, 350)), (self.alchemistry_pos[0] * 60 - 120, self.alchemistry_pos[1] * 60 - 120))
-            game.print_text(25, "Максимальный", (0, 0, 0), (self.alchemistry_pos[0] * 60 - 50, self.alchemistry_pos[1] * 60 - 40))
-            game.print_text(25, "уровень", (0, 0, 0), (self.alchemistry_pos[0] * 60 - 50, self.alchemistry_pos[1] * 60))
-        game.draw_button(100, 35, self.alchemistry_pos[0] * 60 - 40, self.alchemistry_pos[1] * 60 + 120, "Выход", "exit_alchemistry", font_size=25)
+            self.print_text(25, "Максимальный", (0, 0, 0), (self.alchemistry_pos[0] * 60 - 50, self.alchemistry_pos[1] * 60 - 40))
+            self.print_text(25, "уровень", (0, 0, 0), (self.alchemistry_pos[0] * 60 - 50, self.alchemistry_pos[1] * 60))
+        self.render_button(100, 35, self.alchemistry_pos[0] * 60 - 40, self.alchemistry_pos[1] * 60 + 120, "Выход", "exit_alchemistry", font_size=25)
 
     def render_smithy_window(self):
-        if board.return_alchemistry_stage() == 3:
+        if self.board.get_alchemistry_stage() == 3:
             screen.blit(pygame.transform.scale(pygame.image.load(r"textures\scroll.png"), (300, 350)), (self.smithy_pos[0] * 60 - 120, self.smithy_pos[1] * 60 - 120))
-
 
 
 def load_map():
@@ -385,21 +396,21 @@ pygame.display.set_caption("Castle story")
 screen = pygame.display.set_mode(size)
 pygame.display.set_icon(pygame.image.load("textures\icon.png"))
 game = Game()
-board = Board()
 pygame.mixer.music.load("sounds\soundtrack.mp3")
 pygame.mixer.music.play(-1)
 fps = 60
 clock = pygame.time.Clock()
-
 while running:
+    is_click = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT or game.return_exit():
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and not is_click:
+            is_click = True
             game.click()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                board.course_change()
+                game.course_change()
             if event.key == pygame.K_ESCAPE:
                game.click_esc()
     game.render()
